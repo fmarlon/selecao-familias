27bab127e53196826fce3074e41ced91ddbb60e5
Frank Santos <frankmms@gmail.com>
2023-02-14 11:58:59 -0300

    refact(ClassificadorFamilias)
    ajusta retorno de métodos e variáveis no algoritmo de classificação de familias.

--- src/main/java/dev/frankmms/selecaofamilias/ClassificadorFamilias.java
+++ src/main/java/dev/frankmms/selecaofamilias/ClassificadorFamilias.java
@@ -2,12 +2,14 @@ package dev.frankmms.selecaofamilias;
 
 import dev.frankmms.selecaofamilias.model.Familia;
 import dev.frankmms.selecaofamilias.model.FamiliaClassificacao;
+import dev.frankmms.selecaofamilias.model.FamiliaPontuacao;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,33 +18,28 @@ public class ClassificadorFamilias {
     private CalculadoraPontos calculadoraPontos = new CalculadoraPontos();
     
     public List<FamiliaClassificacao> classificar(List<Familia> familias) {
-        List<FamiliaClassificacao> familiasPontuacao = calcularPontuacao(familias);
+        var familiasPontuacao = calcularPontuacao(familias);
 
         ordernarPorPontuacao(familiasPontuacao);
-        definirPosicao(familiasPontuacao);
 
-        return familiasPontuacao;
+        return mapearParaFamiliasClassificacao(familiasPontuacao);
     }
 
-    private void ordernarPorPontuacao(List<FamiliaClassificacao> familiasPontuacao) {
-        familiasPontuacao.sort(Comparator.comparing(it -> -it.getPontuacao().getPontos()));
+    private void ordernarPorPontuacao(List<FamiliaPontuacao> familiasPontuacao) {
+        familiasPontuacao.sort(Comparator.comparing(FamiliaPontuacao::getPontos).reversed());
     }
 
-    private List<FamiliaClassificacao> calcularPontuacao(List<Familia> familias) {
+    private List<FamiliaPontuacao> calcularPontuacao(List<Familia> familias) {
         var familiasPontuacao = familias.stream()
-                .map(familia -> {
-                    var pontuacao = calculadoraPontos.calcular(familia);
-
-                    return new FamiliaClassificacao(pontuacao, 0);
-                })
+                .map(calculadoraPontos::calcular)
                 .collect(Collectors.toList());
         return familiasPontuacao;
     }
 
-    private void definirPosicao(List<FamiliaClassificacao> familiasPontuacao) {
-        for (int i = 0; i < familiasPontuacao.size(); i++) {
-            familiasPontuacao.get(i).setPosicao(i + 1);
-        }
+    private List<FamiliaClassificacao> mapearParaFamiliasClassificacao(List<FamiliaPontuacao> familiasPontuacao) {
+        return IntStream.rangeClosed(1, familiasPontuacao.size())
+                .mapToObj(posicao -> new FamiliaClassificacao(familiasPontuacao.get(posicao - 1), posicao))
+                .collect(Collectors.toList());
     }
 
 }
